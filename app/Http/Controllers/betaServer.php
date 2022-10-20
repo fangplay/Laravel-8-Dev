f49fcb8e0146c4a750be2ac9e48a0fec61d4f692
FANGPLAY SIRIRAK <fangplay_2014@hotmail.com>
2022-10-20 23:42:58 +0700

    add data json response

--- app/Http/Controllers/betaServer.php
+++ app/Http/Controllers/betaServer.php
@@ -17,19 +17,22 @@ class betaServer extends Controller
     function generationData()
     {
         $genData = DB::table('generation')->pluck('name');
-        return view('generations',compact('genData'));
+        // return view('generations',compact('genData'));
+        return $genData->toJson();
     }
     //gamelist route
     function gameData()
     {
         $gameList = DB::table('game-list')->join('generation','game-list.gen_id','=','generation.id')->select('game-list.name as gamename','generation.name as genname')->orderBy('gen_id')->get();
-        return view('game-list',compact('gameList'));
+        // return view('game-list',compact('gameList'));
+        return $gameList->toJson();
     }
     //projects route
     function projects(){
         $projects = DB::table('projects')->join('status','projects.status_id','=','status.id')->select('projects.project_name as pname','status.name as sname')->get();
-        return view('projects',compact('projects'));
+        // return view('projects',compact('projects'));
         // return betaServer::json_encode(array('success'));
+        return $projects->toJson();
     }
     // info route
     function info(){
@@ -39,7 +42,8 @@ class betaServer extends Controller
     // Release Games
     function release(){
         $release = DB::table('game-list')->select('year as Year','name as GameName')->orderBy('year')->get();
-        return view('release',compact('release'));
+        // return view('release',compact('release'));
         // return view('release');
+        return $release->toJson();
     }
 }
